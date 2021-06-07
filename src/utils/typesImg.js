5eb15efc698fc765aa3d3d36306eabad24edbca8
dionkodhyat@pm.me <dionkodhyat@pm.me>
2021-06-07 10:50:36 -0700

    Retrieve types icons from Bulbapedia

---
+++ src/utils/typesImg.js
@@ -0,0 +1,22 @@
+const normal = 'https://cdn2.bulbagarden.net/upload/9/95/Normal_icon_SwSh.png';
+const fighting = 'https://cdn2.bulbagarden.net/upload/3/3b/Fighting_icon_SwSh.png';
+const flying = 'https://cdn2.bulbagarden.net/upload/b/b5/Flying_icon_SwSh.png';
+const poison = 'https://cdn2.bulbagarden.net/upload/8/8d/Poison_icon_SwSh.png';
+const ground = 'https://cdn2.bulbagarden.net/upload/2/27/Ground_icon_SwSh.png';
+const rock = 'https://cdn2.bulbagarden.net/upload/1/11/Rock_icon_SwSh.png';
+const bug = 'https://cdn2.bulbagarden.net/upload/9/9c/Bug_icon_SwSh.png';
+const ghost = 'https://cdn2.bulbagarden.net/upload/0/01/Ghost_icon_SwSh.png';
+const steel = 'https://cdn2.bulbagarden.net/upload/0/09/Steel_icon_SwSh.png';
+const fire = 'https://cdn2.bulbagarden.net/upload/a/ab/Fire_icon_SwSh.png'; 
+const water = 'https://cdn2.bulbagarden.net/upload/8/80/Water_icon_SwSh.png';
+const grass = 'https://cdn2.bulbagarden.net/upload/a/a8/Grass_icon_SwSh.png';
+const electric = 'https://cdn2.bulbagarden.net/upload/7/7b/Electric_icon_SwSh.png';
+const psychic = 'https://cdn2.bulbagarden.net/upload/7/73/Psychic_icon_SwSh.png';
+const ice = 'https://cdn2.bulbagarden.net/upload/1/15/Ice_icon_SwSh.png';
+const dragon = 'https://cdn2.bulbagarden.net/upload/7/70/Dragon_icon_SwSh.png';
+const dark = 'https://cdn2.bulbagarden.net/upload/d/d5/Dark_icon_SwSh.png'; 
+const fairy = 'https://cdn2.bulbagarden.net/upload/c/c6/Fairy_icon_SwSh.png';
+
+const icons = { normal, fighting, flying, poison, ground, rock, bug, ghost, steel, fire, water, grass, electric, psychic, ice, dragon, dark, fairy};
+
+export default icons;
