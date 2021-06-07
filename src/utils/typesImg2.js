f58074a51757e12e3e975ec8494a2abf062d4715
dionkodhyat@pm.me <dionkodhyat@pm.me>
2021-06-07 10:55:08 -0700

    Added a second icon set

---
+++ src/utils/typesImg2.js
@@ -0,0 +1,23 @@
+const normal = 'https://cdn2.bulbagarden.net/upload/0/0f/NormalIC.png';
+const fighting = 'https://cdn2.bulbagarden.net/upload/9/9b/FightingIC.png';
+const flying = 'https://cdn2.bulbagarden.net/upload/d/dc/FlyingIC.png';
+const poison = 'https://cdn2.bulbagarden.net/upload/8/86/PoisonIC.png';
+const ground = 'https://cdn2.bulbagarden.net/upload/8/87/GroundIC.png';
+const rock = 'https://bulbapedia.bulbagarden.net/wiki/File:RockIC.png';
+const bug = 'https://cdn2.bulbagarden.net/upload/b/bd/BugIC.png';
+const ghost = 'https://cdn2.bulbagarden.net/upload/c/c3/GhostIC.png';
+const steel = 'https://cdn2.bulbagarden.net/upload/7/7e/SteelIC.png';
+const fire = 'https://cdn2.bulbagarden.net/upload/9/9f/FireIC.png'; 
+const water = 'https://cdn2.bulbagarden.net/upload/b/b0/WaterIC.png';
+const grass = 'https://cdn2.bulbagarden.net/upload/a/a5/GrassIC.png';
+const electric = 'https://cdn2.bulbagarden.net/upload/e/ea/ElectricIC.png';
+
+const psychic = 'https://cdn2.bulbagarden.net/upload/f/f8/PsychicIC.png';
+const ice = 'https://cdn2.bulbagarden.net/upload/8/86/IceIC.png';
+const dragon = 'https://cdn2.bulbagarden.net/upload/c/c3/DragonIC.png';
+const dark = 'https://cdn2.bulbagarden.net/upload/e/e3/DarkIC.png'; 
+const fairy = 'https://cdn2.bulbagarden.net/upload/3/31/FairyIC.png';
+
+const icons = { normal, fighting, flying, poison, ground, rock, bug, ghost, steel, fire, water, grass, electric, psychic, ice, dragon, dark, fairy};
+
+export default icons;
