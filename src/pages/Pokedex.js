ee1af640c1f0c0321b94b196a61f93856a91dd0a
dionkodhyat@pm.me <dionkodhyat@pm.me>
2021-06-06 20:42:31 -0700

    Implemented helper functions and refactored data

--- src/pages/Pokedex.js
+++ src/pages/Pokedex.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import { refactor } from '../utils/refactor'
 
 const Pokedex = () => {
     const [ query, setQuery ] = React.useState(''); 
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
         axios
           .get(`${URL}/${query.toLocaleLowerCase()}`)
           .then(res => {
-            const result = res.data
+            const result = refactorData(res.data);
+            setResult({ name : result.name,
+                        id : result.id, 
+                        imgSrc : result.imgSrc, 
+                        types : result.types,
+                        stats : result.stats,
+                        abilities : result.abilities, 
+                        height : result.height, 
+                        weight : result.weight });
+            setData({ name : result.name, types : result.types, imgSrc : result.imgSrc})
           })
           .catch(err => {
             console.log(err);
